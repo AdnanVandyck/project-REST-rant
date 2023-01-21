b5b0cf5040b823abdbb4b9e1c65a7854b31685b0
Adnan <adnanvandyck@gmail.com>
2023-01-21 11:42:36 -0500

    Adding home page view

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("YKTV!!")
+    res.render('home')
 })
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
